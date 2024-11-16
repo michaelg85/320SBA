5170d271355af8e90e08b745be192c96408a8849
Michael <Magawrych85@gmail.com>
2024-11-16 01:06:36 -0500

    inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import MovieSelector from "./components/MovieSelector";
 import "./App.css";
 
 function App() {
@@ -6,20 +7,46 @@ function App() {
 
   return (
     <main>
+      {/* Title & description */}
       <div className="title" style={{ color: "yellow" }}>
-        <h1>Movie Picker</h1>
+        <h1>Whose Movie?</h1>
+        <h2>Need help choosing a movie?</h2>
+        <h3>Each enter a movie and let the game decide!</h3>
       </div>
+
+      {/* Red & Blue Columns */}
       <div className="container">
+        {/* Left Column */}
         <div className="column left" style={{ color: "red" }}>
-          {/* Left Column Content */}
           <h2>The Red Corner</h2>
           <p>This is the left column.</p>
+          <form>
+            <input
+              id='text'
+              // onChange={handleChange}
+              // name='searchParams'
+              // type='text'
+              placeholder='Enter your movie...'
+            />
+          </form>
         </div>
+        {/* Right Column */}
         <div className="column right" style={{ color: "skyblue" }}>
-          {/* Right Column Content */}
           <h2>The Blue Corner</h2>
           <p>This is the right column.</p>
+          <form>
+            <input
+              id='text'
+              // onChange={handleChange}
+              // name='searchParams'
+              // type='text'
+              placeholder='Enter your movie...'
+            />
+          </form>
         </div>
+
+
+        
       </div>
     </main>
   );
