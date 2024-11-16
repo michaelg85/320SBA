11b49c49bb23e94fd2363d10282c60c4caad5b95
Michael <Magawrych85@gmail.com>
2024-11-16 00:37:04 -0500

    working

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,23 @@ function App() {
 
   return (
     <main>
-      <h1>Movie Picker</h1>
+      <div className="title" style={{ color: "lightbronze" }}>
+        <h1>Movie Picker</h1>
+      </div>
+      <div className="container">
+        <div className="column left" style={{ color: "red" }}>
+          {/* Left Column Content */}
+          <h2>The Red Corner</h2>
+          <p>This is the left column.</p>
+        </div>
+        <div className="column right" style={{ color: "skyblue" }}>
+          {/* Right Column Content */}
+          <h2>The Blue Corner</h2>
+          <p>This is the right column.</p>
+        </div>
+      </div>
     </main>
   );
 }
 
 export default App;
-
