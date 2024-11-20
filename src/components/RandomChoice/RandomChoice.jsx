3359995090dabd693d15aa3ee5b8914c8afab26e
Michael <Magawrych85@gmail.com>
2024-11-19 19:28:04 -0500

    working on RandomChoice funtion

--- src/components/RandomChoice/RandomChoice.jsx
+++ src/components/RandomChoice/RandomChoice.jsx
@@ -2,15 +2,14 @@ import { useState } from "react";
 import RedMovieDisplay from "../RedMovieDisplay";
 import BlueMovieDisplay from "../BlueMovieDisplay";
 
-const choice1 = RedMovieDisplay;
-const choice2 = BlueMovieDisplay;
-
 const RandomChoice = () => {
-  const choices = [choice1, choice2];
+  const choice1 = RedMovieDisplay;
+  const choice2 = BlueMovieDisplay;
 
   const [selectedChoice, setSelectedChoice] = useState("");
 
   const selectRandomChoice = () => {
+    const choices = [choice1, choice2];
     const randomIndex = Math.floor(Math.random() * choices.length);
     setSelectedChoice(choices[randomIndex]);
   };
@@ -19,9 +18,9 @@ const RandomChoice = () => {
     <div>
       {/* <h1>Random Choice Selector</h1> */}
       <button onClick={selectRandomChoice}>Choose your movie!</button>
-      {selectedChoice && <h2>Winner! {RandomChoice}</h2>}
+      {selectedChoice && <h2>The Champion! {RandomChoice}</h2>}
     </div>
   );
-}
+};
 
 export default RandomChoice;
